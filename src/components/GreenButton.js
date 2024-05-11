dca2aa171cf0f7535b24f0b9a7e808024ad7f63f
Ramanjot Singh <125717171+iamrommy@users.noreply.github.com>
2024-05-11 19:16:48 +0530

    Delete src directory

--- src/components/GreenButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function GreenButton({text}) {
-  return (
-    <div>
-      <button className='h-min rounded px-1 sm:px-4 py-1.5 bg-[#3eb489] hover:bg-[#207a59]'>{text}</button>
-    </div>
-  )
-}
